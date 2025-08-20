53ba5a422095525b5676d153c06f0040830ebadb
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-08-20 10:35:00 +0530

    feat: implement create user and getall manager logic in service layer

--- src/main/java/com/AttendanceServer/repository/UserRepository.java
+++ src/main/java/com/AttendanceServer/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.AttendanceServer.entities.User;
 import com.AttendanceServer.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository <User,Long> {
@@ -11,6 +12,8 @@ public interface UserRepository extends JpaRepository <User,Long> {
     User findByUserRole(UserRole role);
 
     Optional<User> findByEmail(String email);
+
+    List<User> findAllByUserRole(UserRole userRole);
 
 
 
--- src/main/java/com/AttendanceServer/service/AdminService.java
+++ src/main/java/com/AttendanceServer/service/AdminService.java
@@ -1,9 +1,19 @@
 package com.AttendanceServer.service;
 
+import com.AttendanceServer.dto.UserDTO;
+import com.AttendanceServer.entities.Project;
+import com.AttendanceServer.entities.User;
+import com.AttendanceServer.enums.UserRole;
 import com.AttendanceServer.repository.ProjectRepository;
 import com.AttendanceServer.repository.UserRepository;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AdminService {
@@ -13,5 +23,31 @@ public class AdminService {
 
     @Autowired
     private ProjectRepository projectRepository;
+
+    //1 t krne meka - next AdminController
+
+    public UserDTO createUser(UserDTO dto){
+        boolean exists = userRepository.findByEmail(dto.getEmail()).isPresent();
+        if (exists){
+            throw new EntityExistsException("User already exists");
+        }
+        Optional<Project> optionalProject = projectRepository.findById(dto.getProjectId());
+        if (optionalProject.isPresent()){
+            User user = new User();
+            user.setUserRole(dto.getUserRole());
+            user.setPassword(dto.getPassword());
+            user.setEmail(dto.getEmail());
+            user.setName(dto.getName());
+            user.setProject(optionalProject.get());
+            User userCreated = userRepository.save(user);
+            return userCreated.getDto();
+        }
+        throw new EntityNotFoundException("Project Not Found");
+    }
+
+    public List<UserDTO> getAllManagers(){
+        List<User> users = userRepository.findAllByUserRole(UserRole.MANAGER);
+        return  users.stream().map(User::getDto).collect(Collectors.toList());
+    }
 
 }
