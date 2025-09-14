60df4c0529867cfa5664154e56752a8d137245bc
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-09-14 10:23:00 +0530

    feat(api): add method to retrieve all leave requests by employee ID in LeaveRequestRepository

--- src/main/java/com/AttendanceServer/repository/LeaveRequestRepository.java
+++ src/main/java/com/AttendanceServer/repository/LeaveRequestRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
 
     Optional<LeaveRequest> findByEmployeeIdAndProjectIdAndDate(Long employeeId, Long projectId, LocalDate date);
+
+    List<LeaveRequest> findAllByEmployeeId(Long employeeId);
+
 
 }
