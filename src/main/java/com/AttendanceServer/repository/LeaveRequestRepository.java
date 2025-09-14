5c782734ab366b4fee52644ded92634ca7d11021
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-09-14 09:44:27 +0530

    feat(api): add LeaveRequestRepository for leave request data management

---
+++ src/main/java/com/AttendanceServer/repository/LeaveRequestRepository.java
@@ -0,0 +1,10 @@
+package com.AttendanceServer.repository;
+
+import com.AttendanceServer.entities.LeaveRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
+
+}
