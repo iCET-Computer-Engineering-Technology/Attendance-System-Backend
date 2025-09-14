45c11bd6ab8ac287b70c274836d3a2bf68c05e66
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-09-14 09:05:11 +0530

    feat(api): enhance AttendanceService to prevent duplicate attendance entries

--- src/main/java/com/AttendanceServer/repository/AttendanceRepository.java
+++ src/main/java/com/AttendanceServer/repository/AttendanceRepository.java
@@ -4,7 +4,12 @@ import com.AttendanceServer.entities.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
 
+
+    Optional<Attendance> findByEmployeeIdAndProjectIdAndDate(Long employeeId, Long projectId, LocalDate date);
 }
--- src/main/java/com/AttendanceServer/service/AttendanceService.java
+++ src/main/java/com/AttendanceServer/service/AttendanceService.java
@@ -7,6 +7,7 @@ import com.AttendanceServer.entities.User;
 import com.AttendanceServer.repository.AttendanceRepository;
 import com.AttendanceServer.repository.ProjectRepository;
 import com.AttendanceServer.repository.UserRepository;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,22 +28,32 @@ public class AttendanceService {
     private ProjectRepository projectRepository;
 
     public AttendanceDTO markAttendance(AttendanceDTO dto){
-        Optional<User> optionalEmployee = userRepository.findById(dto.getEmployeeId());
-        Optional<User> optionalManager = userRepository.findById(dto.getManagerId());
-        Optional<Project> optionalProject = projectRepository.findById(dto.getProjectId());
 
-        if (optionalEmployee.isPresent() && optionalManager.isPresent() && optionalProject.isPresent()){
-            Attendance attendance = new Attendance();
+        Optional<Attendance> optionalAttendance = attendanceRepository.findByEmployeeIdAndProjectIdAndDate(
+                dto.getEmployeeId(), dto.getProjectId(), LocalDate.now()
+        );
 
-            attendance.setDate(LocalDate.now());
-            attendance.setAttendanceStatus(dto.getAttendanceStatus());
-            attendance.setEmployee(optionalEmployee.get());
-            attendance.setProject(optionalProject.get());
-            attendance.setManager(optionalManager.get());
+        if (optionalAttendance.isEmpty()) {
+            Optional<User> optionalEmployee = userRepository.findById(dto.getEmployeeId());
+            Optional<User> optionalManager = userRepository.findById(dto.getManagerId());
+            Optional<Project> optionalProject = projectRepository.findById(dto.getProjectId());
 
-            return attendanceRepository.save(attendance).getDto();
-        }else {
-            throw new EntityNotFoundException("Some Related Entity Not Found");
+            if (optionalEmployee.isPresent() && optionalManager.isPresent() && optionalProject.isPresent()) {
+                Attendance attendance = new Attendance();
+
+                attendance.setDate(LocalDate.now());
+                attendance.setAttendanceStatus(dto.getAttendanceStatus());
+                attendance.setEmployee(optionalEmployee.get());
+                attendance.setProject(optionalProject.get());
+                attendance.setManager(optionalManager.get());
+
+                return attendanceRepository.save(attendance).getDto();
+            } else {
+                throw new EntityNotFoundException("Some Related Entity Not Found");
+            }
+        }
+        else{
+            throw new EntityExistsException("Attendance Already Marked For Today");
         }
     }
 }
